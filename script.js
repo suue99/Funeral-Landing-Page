2f170ec53b61afb55496e973defe64fe844aed2c
suue99 <susan.olofin@gmail.com>
2024-12-06 01:30:23 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+// const menu = document.querySelector(".menu");
+// const menuItems = document.querySelectorAll(".item");
+// const hamburger = document.querySelector(".hamburger");
+// const closeIcon = document.querySelector(".closeIcon");
+// const menuIcon = document.querySelector(".openIcon");
+
+// function toggleMenu() {
+//   if (menu.classList.contains("showMenu")) {
+//     menu.classList.remove("showMenu");
+//     closeIcon.style.display = "none";
+//     openIcon.style.display = "block";
+//   } else {
+//     menu.classList.add("showMenu");
+//     closeIcon.style.display = "block";
+//     openIcon.style.display = "none";
+//   }
+// }
+
+// hamburger.addEventListener("click", toggleMenu);
