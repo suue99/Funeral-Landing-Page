216a478d2313b7577f2ea3b85869d4ac67672cc0
LCA Tech <tech@lcalondon.org>
2024-12-13 16:41:52 +0000

    EO-MinorChanges

--- nav/header.php
+++ nav/header.php
@@ -1,45 +1,40 @@
 <header class="header">
 
-
-<nav>
-  <ul class="main-nav-list">
-    <li><a class="main-nav-link" href="index.php">Home</a></li>
-    <li class="dropdown">
-      <a class="main-nav-link" href="javascript:void(0)">Tributes</a>
-      <ul class="dropdown-menu">
-        <li><a href="tributes.php">View Tributes</a></li>
-        <li><a href="sharememory.php">Submit Tribute</a></li>
-      </ul>
-    </li>
-  </ul>
-</nav>
-
-
- <!-- Google Fonts -->
- <link rel="preconnect" href="https://fonts.googleapis.com">
-  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-  <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet"/>
-  <link
-      href="https://fonts.googleapis.com/css2?family=Sacramento&display=swap"
-      rel="stylesheet"
-    />
-    <link
-      href="https://fonts.googleapis.com/css2?family=Sofia&display=swap"
-      rel="stylesheet"
-    />  
-  
-  <link rel="stylesheet" href="css/tributes.css">
-
-  <link rel="stylesheet" href="css/style.css" />
-  <link rel="stylesheet" href="css/query.css" />
-  <link rel="stylesheet" href="css/form.css" />
-
-
+  <!-- Hamburger Menu Button -->
   <button class="btn-mobile-nav">
-    <ion-icon class="icon-mobile-nav" name="menu-outline"></ion-icon>
-    <ion-icon class="icon-mobile-nav" name="close-outline"></ion-icon>
+    <ion-icon name="menu-outline" class="icon-mobile-nav"></ion-icon>
+    <ion-icon name="close-outline" class="icon-mobile-nav" style="display: none;"></ion-icon>
   </button>
+
+  <!-- Navigation -->
+  <nav>
+    <ul class="main-nav-list">
+      <li><a class="main-nav-link" href="index.php">Home</a></li>
+      <li class="dropdown">
+        <a class="main-nav-link" href="javascript:void(0)">Tributes</a>
+        <ul class="dropdown-menu">
+          <li><a href="tributes.php">View Tributes</a></li>
+          <li><a href="sharememory.php">Submit Tribute</a></li>
+        </ul>
+      </li>
+    </ul>
+  </nav>
+
+  <!-- Google Fonts -->
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+  <link href="https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,100..900;1,100..900&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Sacramento&display=swap" rel="stylesheet">
+  <link href="https://fonts.googleapis.com/css2?family=Sofia&display=swap" rel="stylesheet">
+
+  <!-- Stylesheets -->
+  <link rel="stylesheet" href="css/tributes.css">
+  <link rel="stylesheet" href="css/style.css">
+  <link rel="stylesheet" href="css/query.css">
+  <link rel="stylesheet" href="css/form.css">
+  <link rel="stylesheet" href="css/mobile-nav.css">
+
+  <!-- JavaScript -->
+  <script defer src="script.js"></script>
+
 </header>
-
-
-  
